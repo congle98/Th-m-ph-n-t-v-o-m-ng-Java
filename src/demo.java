8ee73abfbd48e847d0921ad937f3039c428b6e6d
congle98 <lethanhcongcnhn@gmail.com>
2021-04-05 14:49:32 +0700

    addfile

--- src/demo.java
+++ src/demo.java
@@ -1,6 +1,22 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
 public class demo {
+    static void menu(Scanner scanner, int arr[]){
+        while (true){
+            System.out.println("chọn menu 1.Thêm \n 2.hiển thị mảng \n 3.thoát");
+            int menu = scanner.nextInt();
+            switch (menu){
+                case 1:
+                    addArr(scanner,arr);
+                    break;
+                case 2:
+                    getArr(arr);
+                    break;
+                case 3:
+                    System.exit(0);
+            }
+        }
+    }
     static void addArr(Scanner scanner,int arr[]){
         System.out.println("Phần tử muốn thêm là:");
         int x = scanner.nextInt();
@@ -23,23 +39,18 @@ public class demo {
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Mời nhập số lượng phần tử trong mảng: ");
-        int n = scanner.nextInt();
-        int arr[] =new int[n];
-        while (true){
-            System.out.println("chọn menu 1.Thêm \n 2.hiển thị mảng \n 3.thoát");
-            int menu = scanner.nextInt();
-            switch (menu){
-                case 1:
-                    addArr(scanner,arr);
-                    break;
-                case 2:
-                    getArr(arr);
-                    break;
-                case 3:
-                    System.exit(0);
+        int n;
+        do {
+            System.out.println("Mời nhập số lượng phần tử trong mảng: ");
+            n = scanner.nextInt();
+            if(n<1){
+                System.out.println("số lượng phải lớn hơn 1");
             }
-        }
+        }while (n<1);
+        int arr[] =new int[n];
+        menu(scanner,arr);
+
+
 
 
     }
