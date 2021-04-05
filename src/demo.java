251224e48f05bba8112cc82f8baec613aa24216e
congle98 <lethanhcongcnhn@gmail.com>
2021-04-05 14:30:40 +0700

    addfile

---
+++ src/demo.java
@@ -0,0 +1,46 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class demo {
+    static void addArr(Scanner scanner,int arr[]){
+        System.out.println("Phần tử muốn thêm là:");
+        int x = scanner.nextInt();
+        int i;
+        do {
+            System.out.println("Vào vị trí thứ:");
+             i = scanner.nextInt();
+             if((i<0) || (i>arr.length-1)){
+                 System.out.println("i phải nằm trong khoảng từ 0 đến"+arr.length);
+             }
+        }while ((i<0) || (i>arr.length-1));
+        arr[i]=x;
+        System.out.println("đã thêm "+x+" vào vị trí thứ "+i);
+
+    }
+    static void getArr(int arr[]){
+        for(int i=0; i<arr.length;i++){
+            System.out.println("Vị trí thứ "+i+" là "+arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Mời nhập số lượng phần tử trong mảng: ");
+        int n = scanner.nextInt();
+        int arr[] =new int[n];
+        while (true){
+            System.out.println("chọn menu 1.Thêm \n 2.hiển thị mảng \n 3.thoát");
+            int menu = scanner.nextInt();
+            switch (menu){
+                case 1:
+                    addArr(scanner,arr);
+                    break;
+                case 2:
+                    getArr(arr);
+                    break;
+                case 3:
+                    System.exit(0);
+            }
+        }
+
+
+    }
+}
